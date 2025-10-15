bb32600effb3937d3c093c3962bfd5ceafa776dc
Sam Fu <sam@xapa.com>
2025-10-15 12:20:30 +0800

    feat: update lincense authors

--- msa/vc-executor/src/main/java/org/thingsboard/server/vc/ThingsboardVersionControlExecutorApplication.java
+++ msa/vc-executor/src/main/java/org/thingsboard/server/vc/ThingsboardVersionControlExecutorApplication.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 package org.thingsboard.server.vc; /**
- * Copyright © 2016-2022 The Thingsboard Authors
+ * Copyright © 2016-2022 The Sensi Authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
--- ui-ngx/src/app/modules/home/components/widget/lib/indicator/signal-strength-widget.component.ts
+++ ui-ngx/src/app/modules/home/components/widget/lib/indicator/signal-strength-widget.component.ts
@@ -16,7 +16,7 @@
 
 
 ///
-/// Copyright © 2016-2023 The Thingsboard Authors
+/// Copyright © 2016-2023 The Sensi Authors
 ///
 /// Licensed under the Apache License, Version 2.0 (the "License");
 /// you may not use this file except in compliance with the License.
